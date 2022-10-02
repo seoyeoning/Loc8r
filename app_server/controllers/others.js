48b7547082dd5f9f63a484024d297412221a8d24
seoyeoning <minalunpark@naver.com>
2022-10-02 19:30:28 +0900

    실습 5-6

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,6 +1,6 @@
 /* GET about page. */
 const about = (req, res) => {
-  res.render("index", { title: "About   by 박서연_2020710026" });
+  res.render("generic-text", { title: "About   by 박서연_2020710026" });
 };
 
 module.exports = {
