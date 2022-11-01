e6312622168dc8e2afc4b77d4ea08689cbef182c
seoyeoning <minalunpark@naver.com>
2022-11-02 02:02:55 +0900

    ch1~ch13

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+require("./app_api/models/db");
 
 //var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 const indexRouter = require("./app_server/routes/index");
 const usersRouter = require("./app_server/routes/users");
+const apiRouter = require("./app_api/routes/index");
 
 var app = express();
 
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,22 +4,9 @@ const router = express.Router();
 const ctrlLocations = require("../controllers/locations");
 const ctrlOthers = require("../controllers/others");
 
-/* GET home page. */
-/* router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express &Nodemon by 박서연_2020710026" });
-}); */
-
-/* const homepageController = (req, res) => {
-  res.render("index", { title: "Express &Nodemon by 박서연_2020710026" });
-};
- */
-
-/* GET home page. */
-/* router.get("/", ctrlMain.index); */
-
 /* Locations pages */
 router.get("/", ctrlLocations.homelist);
-router.get("/location", ctrlLocations.locationInfo);
+router.get("/location/:locationid", ctrlLocations.locationInfo);
 router.get("/location/review/new", ctrlLocations.addReview);
 
 /* Other pages */
